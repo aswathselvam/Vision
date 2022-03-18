db7a69f22385fe873339b9e162a48b5a64b5d956
aswathselvam <m.aswath08@gmail.com>
2022-03-17 23:23:03 -0400

    Initial commit

---
+++ MeanShift/main.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+class MeanShift:
+    def __init__():
+        pass
+    
+    def classify():
+        pass
+
+
+
+image = cv2.imread('./road_intersection.jpg')
+image = cv2.resize(image,(500,500))
+cv2.imshow("image",image)
+cv2.waitKey(0)
